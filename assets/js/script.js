717de31268248eced297f62ac9d2b4f6ec526967
Ben <byeungis@gmail.com>
2022-05-09 21:05:30 -0400

    added files

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+let questions =[
+    {
+        numb:1,
+        question: "How would you console log the computer's RAM?",
+        answer: "console.log(computer.ram);",
+        options: [
+            "console.log(computer[ram]);",
+            "console.log(computer.ram);",
+            "console.log(computer(ram));",
+            "console.log(computer->ram)"
+        ]
+    },
+    {
+        numb:2,
+        question:"How would you call the object's startup method?",
+        answer: "computer.startUp();",
+        options: [
+            "computer.startUp;",
+            "computer.startup();",
+            "computer.startUp();",
+            "computer[upstart]"
+        ]
+    },
+    {
+        numb:3,
+        question:"Box-sizing is a property that changes the calulation of which values of the element?",
+        answer: "Both width and height",
+        options: [
+            "width",
+            "height",
+            "margin",
+            "Both width and height"
+        ]
+    },
+    {
+        numb:4,
+        question:"If you need to position a child element in relation to its parent element, which value should you set the position property to?",
+        answer: "absolute",
+        options: [
+            "display",
+            "static",
+            "absolute",
+            "relative"
+        ]
+    },
+    {
+        numb:5,
+        question:"The main purpose of the <body> element is to hold all of the document's actual content that is meant to be seen or interaced with by the page's visitor.",
+        answer: "true",
+        options: [
+            "true",
+            "false"
+        ]
+    }
+  ];
