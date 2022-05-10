9ee38eebdb13a0e1f97b3eef872b1c1222d04e62
Ben <byeungis@gmail.com>
2022-05-09 22:03:24 -0400

    added files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,212 @@
-let questions =[
+const timerElement = document.getElementById("time-remaining");
+const timerView = document.getElementById("timer");
+const highScoreView = document.querySelector("#highscores");
+const startButton = document.getElementById("start-quiz");
+
+const mainElement = document.querySelector("#main-content");
+const messageElement = document.querySelector("h1");
+const textElement = document.querySelector("p");
+
+const choicesListElement = document.getElementById("choices-list");
+const indicatorElement = document.getElementById("indicator");
+
+const formElement = document.createElement("div");
+const highscoresElement = document.createElement("div");
+const textInputElement = document.createElement("input");
+const formButton = document.createElement("button");
+const backButton = document.createElement("button");
+const clearButton = document.createElement("button");
+
+
+var highscore = {
+    initials: "",
+    score: 0,
+};
+var highscores = [];
+var secondsLeft;
+var timerInterval;
+
+var questions =[
     {
-        numb:1,
         question: "How would you console log the computer's RAM?",
-        answer: "console.log(computer.ram);",
-        options: [
+        choices: [
             "console.log(computer[ram]);",
             "console.log(computer.ram);",
             "console.log(computer(ram));",
             "console.log(computer->ram)"
-        ]
+        ],
+        answer: 1,
     },
     {
-        numb:2,
         question:"How would you call the object's startup method?",
-        answer: "computer.startUp();",
-        options: [
+        choices: [
             "computer.startUp;",
             "computer.startup();",
             "computer.startUp();",
             "computer[upstart]"
-        ]
+        ],
+        answer: "computer.startUp();",
     },
     {
-        numb:3,
         question:"Box-sizing is a property that changes the calulation of which values of the element?",
-        answer: "Both width and height",
-        options: [
+        choices: [
             "width",
             "height",
             "margin",
             "Both width and height"
-        ]
+        ],
+        answer: 3,
     },
     {
-        numb:4,
         question:"If you need to position a child element in relation to its parent element, which value should you set the position property to?",
-        answer: "absolute",
-        options: [
+        choices: [
             "display",
             "static",
             "absolute",
             "relative"
-        ]
+        ],
+        answer: 2,
     },
     {
-        numb:5,
         question:"The main purpose of the <body> element is to hold all of the document's actual content that is meant to be seen or interaced with by the page's visitor.",
-        answer: "true",
-        options: [
+        choices: [
             "true",
             "false"
-        ]
+        ],
+        answer: 0,
     }
-  ];
+];
+
+
+// FUNCTIONS
+init();
+//starting with the timer and score
+function init() {
+    score = 0;
+    secondsLeft = 60;
+}
+//when pressing the startbutton, removes the main body elements and the timer
+function startGame() {
+    startButton.remove();
+    textElement.remove();
+    timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerElement.textContent = secondsLeft;
+
+        if (secondsLeft <= 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+
+    renderQuiz();
+}
+
+function renderQuiz(questionNumber) {
+    questionNumber = questionNumber || 0;
+    var questionItem = questions[questionNumber];
+    messageElement.textContent = questionItem.question;
+
+    var newChoices = document.createElement("div");
+    choicesListElement.appendChild(newChoices);
+
+    for (var i = 0; i < questionItem.choices.length; i++) {
+        var choice = questionItem.choices[i];
+
+        var li = document.createElement("li");
+        li.setAttribute("data-index", i);
+        li.textContent = choice;
+        newChoices.appendChild(li);
+
+        li.addEventListener("click", function (event) {
+            if (
+                questionItem.answer ===
+                parseInt(event.target.getAttribute("data-index"))
+            ) {
+                score += 10;
+                indicatorElement.innerHTML = "<hr> CORRECT!";
+                indicatorElement.setAttribute("style", "color: lightgreen");
+            } else {
+                secondsLeft -= 10;
+                indicatorElement.innerHTML = "<hr> WRONG!";
+                indicatorElement.setAttribute("style", "color: red");
+            }
+
+            questionNumber++;
+
+            if (questionNumber === questions.length) {
+                clearInterval(timerInterval);
+                indicatorElement.textContent = "";
+                newChoices.remove();
+                messageElement.textContent = "Game Over!";
+                messageElement.appendChild(textElement);
+                textElement.textContent = "Your final score is: " + score;
+
+                renderForm();
+            } else {
+                setTimeout(function () {
+                    renderQuiz(questionNumber);
+                    newChoices.remove();
+                    indicatorElement.textContent = "";
+                }, 1000);
+            }
+        });
+    }
+}
+
+function renderForm() {
+    formElement.textContent = "ENTER NAME: ";
+    formElement.setAttribute("style", "color: white");
+    formButton.textContent = "SUBMIT";
+    mainElement.appendChild(formElement);
+    formElement.appendChild(textInputElement);
+    formElement.appendChild(formButton);
+}
+
+function submitHighscore() {
+    var initialInput = document.querySelector("input").value;
+    highscore.initials = initialInput;
+    highscore.score = score;
+    console.log(highscore);
+    localStorage.setItem("highscore", JSON.stringify(highscore));
+    mainElement.innerHTML = "";
+    highScoreView.textContent = "";
+    timerView.textContent = "";
+
+    renderHighscores();
+}
+
+function renderHighscores() {
+    var storedHighscore = JSON.parse(localStorage.getItem("highscore"));
+    console.log(storedHighscore);
+    messageElement.innerHTML = "Highscores";
+    messageElement.setAttribute("style", "color: white");
+    mainElement.appendChild(messageElement);
+    console.log(storedHighscore.initials);
+    console.log(storedHighscore.score);
+    highscoresElement.setAttribute("class", "highscore-element");
+    highscoresElement.textContent = `${storedHighscore.initials} - ${storedHighscore.score}`;
+    messageElement.appendChild(highscoresElement);
+    backButton.textContent = "Home";
+    clearButton.textContent = "Clear";
+    mainElement.appendChild(backButton);
+    mainElement.appendChild(clearButton);
+}
+
+function clear() {
+    highscoresElement.remove();
+}
+
+function home() {
+    location.reload();
+}
+
+highScoreView.addEventListener("click", function () {
+    textElement.remove();
+    startButton.remove();
+    renderHighscores();
+});
+
+startButton.addEventListener("click", startGame);
+formButton.addEventListener("click", submitHighscore);
+backButton.addEventListener("click", home);
+clearButton.addEventListener("click", clear);
